4edc887a6036c34ca6ff8bdd127a35c1a27a0b8d
Tim David Harris <timdavidharris@gmail.com>
2021-07-15 16:53:57 -0600

    Wrote code in the function to work with all but the last text

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+let string = '';
+let runTimes = 0;
 
+const repeatString = function(string, runTimes) {
+    if (runTimes < 0){
+        return 'ERROR';
+    } else if (runTimes === 0) {
+        return '';
+    } else {
+    for (let i = 1; i < runTimes; i++){
+        string += 'hey';
+    }
+}
+return string;
 };
 
 module.exports = repeatString;
