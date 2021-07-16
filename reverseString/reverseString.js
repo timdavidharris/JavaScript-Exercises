06f39181dbe567af17fad08d15b4ef6c50b72026
Tim David Harris <timdavidharris@gmail.com>
2021-07-15 19:32:59 -0600

    My solution that passes all the tests

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+let theString = '';
 
+const reverseString = function(theString) {
+    let theNewString = theString.split('');
+    let reverseString = theNewString.reverse();
+    let output = reverseString.join('');
+    return output;
 };
 
 module.exports = reverseString;
