1b12f9c578257f07e054184821811278fb537c82
Tim David Harris <timdavidharris@gmail.com>
2021-07-16 18:29:27 -0600

    Working on the solutions

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,8 +1,17 @@
 let array = [];
-let takeOut = '';
+let takeOut1 = '';
+let takeOut2 = '';
 
-const removeFromArray = function(array, takeOut) {
-    
+const removeFromArray = function(array, takeOut1, takeOut2) {
+    if ((takeOut1 !== '') || (takeOut1 !== null)) {
+        let remove1 = array.indexOf(takeOut1);
+        array.splice(remove1, 1);
+    }
+        if ((takeOut2 !== '') || (takeOut2 !== null)) {
+            let remove2 = array.indexOf(takeOut2);
+            array.splice(remove2, 1);
+        }
+    return array;
 };
 
 module.exports = removeFromArray;
