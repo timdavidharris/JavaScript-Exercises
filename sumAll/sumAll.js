56a71351a190d522b4e34c77141b2df31fbf4a16
Tim David Harris <timdavidharris@gmail.com>
2021-07-16 19:09:57 -0600

    My solution to this exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let addItUp = null;
+    if (typeof num1 === 'string' || typeof num2 === 'string') {
+        return 'ERROR'
+    } else if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+        if (num1 < num2) {
+            while (num1 <= num2) {
+                addItUp += num1;
+                num1++;
+            }
+            return addItUp;
+        } else if (num2 < num1) {
+            while (num2 <= num1) {
+                addItUp += num2;
+                num2++;
+            }
+            return addItUp;
+        } 
+    else {
+        return 'ERROR';
+    }
 };
 
 module.exports = sumAll;
