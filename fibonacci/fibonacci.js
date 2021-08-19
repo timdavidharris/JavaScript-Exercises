27eaf19c3264a69f487ff9576ab16a3adfed7ad3
Tim David Harris <timdavidharris@gmail.com>
2021-08-19 17:19:00 -0600

    my solution code for pulling from the fibonacci sequence

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return 'OOPS';
+    }
+    let fib = [0, 1];
+    for (let i = (+num); i > 0; i--) {
+        let length = fib.length;
+        fib[length] = fib[length - 2] + fib[length - 1];
+    }
+    return (fib[num]);
 };
+
+fibonacci(4);
 
 module.exports = fibonacci;
