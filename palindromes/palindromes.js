803693ee77b5c3002d34cf2e1f42e4dbd5a862b6
Tim David Harris <timdavidharris@gmail.com>
2021-08-19 16:42:14 -0600

    my solution to checking for palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let array = [];
+    array = word;
+    array = array.replace(/[^\w\s]|_/g, "")
+                 .replace(/\s+/g, "")
+                 .toLowerCase()
+    let reverseArray = array.split('').reverse().join('');
+    return reverseArray === array;
 };
 
 module.exports = palindromes;
