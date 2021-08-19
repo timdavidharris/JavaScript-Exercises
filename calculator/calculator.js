ceec03060301e43ba198f7f8271acaa9fa82ee2b
Tim David Harris <timdavidharris@gmail.com>
2021-08-18 20:20:53 -0600

    my solutions for this exercise

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1; 
+  }
+  let product = 1;
+  for (let i = num; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 module.exports = {
