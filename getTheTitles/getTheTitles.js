71739f60d3b93ad3ffe55cef980525f73b8b87d2
Tim David Harris <timdavidharris@gmail.com>
2021-08-20 19:51:13 -0600

    The solution code

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(array) {
+    let findTitles = array.map(book => book.title);
+    return findTitles;
+}
 
 module.exports = getTheTitles;
